b71cd1a8830c888ac88b97e7b79b86646b7a887f
leolil1 <123527481+leolil1@users.noreply.github.com>
2024-05-29 00:17:12 -0400

    Update Controller.v
    Added the 3 "Load" instructions from week6 lecture video "Instruction Set"

--- Components/Controller.v
+++ Components/Controller.v
@@ -5,7 +5,7 @@
 module Controller(
   input clk, reset,
   input [3:0] Opcode,
-  output reg LoadIR, IncPC, SelPC, LoadPC, LoadReg, LoadAcc,
+  output reg LoadIR, IncPC, SelPC, LoadPC, LoadReg, DumpReg, LoadAcc,
   output reg [1:0] SelAcc,
   output reg [3:0] SelALU
 );
@@ -18,7 +18,7 @@ module Controller(
     if (reset==1)begin
       //count<=4'b0;
       stage<=2'b00;
-      LoadIR<=0; IncPC<=0; SelPC<=0; LoadPC<=0; LoadReg<=0; LoadAcc<=0;
+      LoadIR<=0; IncPC<=0; SelPC<=0; LoadPC<=0; LoadReg<=0; DumpReg<=0; LoadAcc<=0;
       SelAcc<=2'b0;
       SelALU<=4'b0;
     end
@@ -28,41 +28,73 @@ module Controller(
     //This means, later when we are putting things together, we need to connect IR to Controller.
    if (stage==2'b00)begin
 	//fetch instruction
-  LoadIR<=1;             //Send out the load instruction signal to IR.
+  	LoadIR<=1;             //Send out the load instruction signal to IR.
 	IncPC<=1;              //Incrementing Program Counter here? or no?
 	SelPC<=0; 
 	LoadPC<=0; 
-	LoadReg<=0; 
+	LoadReg<=0;
+	DumpReg<=1;
 	LoadAcc<=0;
 	SelAcc<=0;
 	SelALU<=0;
-  stage<=2'b01;         //Once instruction is loaded, we now move on to stage 1.
+  	stage<=2'b01;         //Once instruction is loaded, we now move on to stage 1.
 	end
    //stage 1 block.
    //In this stage, we are generating different control signals based on the Opcode.
    //So there will be many differnt case statements.
    else if (stage==2'b01)begin
     case(Opcode)
-    //Load instruction
-    4'b0001:begin           
-	LoadIR<=0; 
-	IncPC<=0; 
+    4'b0100:begin
+   	//Load Reg to ACC.  
+  	LoadIR<=0; 
+	IncPC<=1; 
 	SelPC<=0; 
 	LoadPC<=0; 
-	LoadReg<=1; 
+	LoadReg<=0;
+    	DumpReg<=1;
 	LoadAcc<=0;
-	SelAcc<=0;
-	SelALU<=0;
-	stage<=2'b10;
+	SelAcc<=2'b00;
+	SelALU<=0;  
+	stage<=2'b10; 
     end
-    
+
+    4'b0101:begin
+   	//Load ACC to Reg.  
+  	LoadIR<=0; 
+	IncPC<=1; 
+	SelPC<=0; 
+	LoadPC<=0; 
+	LoadReg<=0;
+    	DumpReg<=0;
+	LoadAcc<=1;
+	SelAcc<=2'b00;
+	SelALU<=0;  
+	stage<=2'b10; 
+    end
+
+     4'b0101:begin
+   	//Load Immediate to ACC.  
+  	LoadIR<=0; 
+	IncPC<=1; 
+	SelPC<=0; 
+	LoadPC<=0; 
+	LoadReg<=0;
+    	DumpReg<=0;
+	LoadAcc<=0;
+	SelAcc<=2'b01;  //So I'm thinking IR needs to have the Immediate_data output be connected to the first MUX for ACC, and also the MUX to program counter. 
+	//Then depends on the selection, the immedaite_data can be accepted or ignored. Here, it'll be accepted.
+	SelALU<=0;  
+	stage<=2'b10; 
+    end
+	    
     4'b0010:begin
 	//Add instruction.  
-  LoadIR<=0; 
+  	LoadIR<=0; 
 	IncPC<=0; 
 	SelPC<=0; 
 	LoadPC<=1; 
-	LoadReg<=0; 
+	LoadReg<=0;
+	DumpReg<=1;
 	LoadAcc<=0;
 	SelAcc<=0;
 	SelALU<=4'b0001;   //I'm assuming SelALU for ADD is 0001. Need to find this info from Andrew/James' code.
