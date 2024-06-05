9c2f4c49b8eabd1d4312f254c8d9338b19caa248
leolil1 <123527481+leolil1@users.noreply.github.com>
2024-06-05 00:18:04 -0400

    Update RegisterTest.v
    Updated testbench to now use RegisterFile.

--- TestBench/RegisterTest.v
+++ TestBench/RegisterTest.v
@@ -3,22 +3,30 @@ module RegisterTest();
   reg reset;
   reg LoadReg;
   reg DumpReg;
+  reg [3:0] RegNumber;
   reg [7:0] in;
   wire [7:0] out;
   wire [7:0] out_alu;
 
-   
-  Register MUT(clk, reset, LoadReg, DumpReg, in, out, out_alu);
+    // Correct module instantiation line
+    RegisterFile MUT(clk, reset, LoadReg, DumpReg, RegNumber, in, out, out_alu);
 	
-	always #5 clk = ~clk;
+    always #10 clk = ~clk;
     initial
     begin
         clk=0;
-        #5 reset = 1'b0;
+        reset = 1'b1;
+	#5reset = 1'b0;
 
-	      #20 LoadReg = 1'b1;
-	       in=8'b11001111;
+	#5LoadReg = 1'b1;
+	 in=8'b11001100;
+	 RegNumber=4'b0001;
+	#5 DumpReg = 1'b1;
 
-	      #5 DumpReg = 1'b1;
+	#5LoadReg = 1'b1;
+	 in=8'b11110000;
+	 RegNumber=4'b1111;
+	#5 DumpReg = 1'b1;
+	
     end
 endmodule
