46588203d133198968e8cf7313b2bfc51f224de9
leolil1 <123527481+leolil1@users.noreply.github.com>
2024-06-14 12:52:29 -0400

    Update InstructionRegister.v
    Added a Load Instruction output to load instruction without PC for now.

--- Components/InstructionRegister.v
+++ Components/InstructionRegister.v
@@ -6,14 +6,20 @@ module IR(
   input LoadIR,
   input [7:0] instruction,
   output [7:0] Opcode,
+  output reg LoadIRSig         //a Load Instruction signal which will be sent to testbench to load an instruction. 
+                               //This will be removed if we have the PC setup.
 );
   reg [7:0] storage;
   
   always @(posedge clk, reset)begin
     if (reset==1)
-      storage<=8'bz;
-    else if(LoadIR)
+      storage<=8'b0;
+    else if(LoadIR)begin
+      LoadIRSig<=1;
       storage<=instruction;
+    end else begin
+	    LoadIRSig<=0;
+    end
   end
   
   assign Opcode=storage[7:0];
