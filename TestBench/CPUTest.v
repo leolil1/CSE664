826bfbbccb8027d61687d8fa0ada442391fcad89
leolil1 <123527481+leolil1@users.noreply.github.com>
2024-06-14 12:54:29 -0400

    Update CPUTest.v

--- TestBench/CPUTest.v
+++ TestBench/CPUTest.v
@@ -9,6 +9,8 @@ module CPUTest();
     
     initial begin
         //Enter all the instrucitons wish to execute in a sequence.
+	//first 4-bit is the Opcode. Reference the Controller to
+	//locate the Opcode of the list of instructions we can run.
         memory[0] = 8'b11010001;
         memory[1] = 8'b01010001;
         memory[2] = 8'b11010010;
